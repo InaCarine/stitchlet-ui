0ee2d9feedc13280b27940de4ddeb9c18a6b8806
InaCarine <29457046+InaCarine@users.noreply.github.com>
2025-12-12 22:11:18 +0000

    chore: add initial Storybook configuration and example stories

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,4 +1,11 @@
 import { config } from '@repo/eslint-config/react-internal';
+import storybook from 'eslint-plugin-storybook';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default config;
+export default [
+  ...config,
+  {
+    plugins: { storybook },
+    rules: { ...storybook.configs.recommended.rules },
+  },
+];
