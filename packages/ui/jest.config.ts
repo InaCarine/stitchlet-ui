34ec8b4832bb96b9e73da43c2f40e574e791f528
InaCarine <29457046+InaCarine@users.noreply.github.com>
2026-02-10 16:40:28 +0000

    chore: clean up jest config

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -8,31 +8,20 @@ import type { Config } from 'jest';
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!**/vendor/**'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/coverage',
-    'package.json',
-    'package-lock.json',
-    'jest.setup.ts',
-    'tsconfig.json',
-    '/.turbo/',
-    'eslint.config.mjs',
-  ],
   coverageProvider: 'v8',
-  // A set of global variables that need to be available in all test environments
-  // globals: {},
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.turbo/', '/dist/'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/src/**/*.test.(ts|tsx)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileTransformer.js',
   },
 };
 
