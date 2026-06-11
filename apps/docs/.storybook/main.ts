2fd38699078792058f8d7cd7afeb7464572817e3
InaCarine <29457046+InaCarine@users.noreply.github.com>
2026-06-11 11:40:31 +0100

    chore: upgrade pnpm to 11

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -19,5 +19,8 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-onboarding'),
   ],
   framework: getAbsolutePath('@storybook/react-vite'),
+  core: {
+    disableTelemetry: true,
+  },
 };
 export default config;
